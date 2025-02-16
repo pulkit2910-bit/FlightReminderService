f729d425cca84c74724d3c2e994507bd0af1a11d
pulkit2910-bit <ranjanpulkit2910@gmail.com>
2025-02-17 00:09:12 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+
+const { sendBasicEmail } = require('./services/email-service');
+
+const setupAndStartServer = () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'moviebookingappservice@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I hope you like the support'
+        );
+    });
+}
+
+setupAndStartServer();
