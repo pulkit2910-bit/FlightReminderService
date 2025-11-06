da97917ef98f386240b9370fcd44d77c52cd6838
Pulkit <ranjanpulkit2910@gmail.com>
2025-11-06 21:54:44 +0530

    endpoint change

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const NotificationController = require('../../controllers/notification');
 
 router.post("/sendmail", NotificationController.sendBasicEmail);
-router.get("/notification/:id", NotificationController.getNotification);
-router.get("/notifications", NotificationController.getAllNotifications);
-router.post("/notification", NotificationController.createNotification);
-router.get("/notifications/pending", NotificationController.fetchPendingNotifications);
+router.get("/:id", NotificationController.getNotification);
+router.get("/all", NotificationController.getAllNotifications);
+router.post("/", NotificationController.createNotification);
+router.get("/pending", NotificationController.fetchPendingNotifications);
 
 module.exports = router;
